c08fcc1b4430a30ba4cb67abd9e6a427ede7b8f1
RadJKW <jwest@howard.com>
2022-06-22 11:46:02 -0500

    reduced print bloat

--- instructionsDisplay/mqtt_controller.py
+++ instructionsDisplay/mqtt_controller.py
@@ -14,38 +14,35 @@
 
 # This example shows how you can use the MQTT client in a class.
 
-#from context import context  # Ensures paho is in PYTHONPATH
+# from context import context  # Ensures paho is in PYTHONPATH
 
 import socket
 import threading
 import time
 import paho.mqtt.client as mqtt
 import json
-import pprint as pp 
+import pprint as pp
 import os
 
 
 class MyMQTTClass(mqtt.Client):
-
     def on_connect(self, mqttc, obj, flags, rc):
-        pass
-        # if rc == 0:
-        #     print("Connected to MQTT Broker!")
-        # else:
-        #     print("Failed to connect, return code %d\n", rc)
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
 
     def on_connect_fail(self, mqttc, obj):
-        pass
-        # print("Connect failed")
+        print("Connect failed")
 
     def on_message(self, mqttc, obj, msg):
         # if message is a json string, convert to dict and
         print("MESSAGE RECEIVED: {}".format(msg.topic))
-        if msg.payload is not None:            
+        if msg.payload is not None:
             msg.payload = json.loads(msg.payload)
             pp.pprint(msg.payload, indent=2, sort_dicts=False, compact=False)
             return
-        print(msg.topic+" "+str(msg.qos)+" "+str(msg.payload))
+        print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
 
     def on_publish(self, mqttc, obj, mid):
         pass
@@ -60,44 +57,25 @@ class MyMQTTClass(mqtt.Client):
         # if logging level is MQTT_LOG_ERROR
         if level == mqtt.MQTT_LOG_ERROR:
             print("LOGGING: {}".format(string))
-        
-        
 
-    # def run(self):
-    #     self.connect(self.broker, self.timeout)
-    #     self.subscribe("$SYS/#", 0)
-    #     self.publish(topic = "pi/pythonTest", payload = "Hello World!", qos = 2, retain = False)
-
-    #     rc = 0
-    #     while rc == 0:
-    #         rc = self.loop()
-    #     return rc
-
-
-# If you want to use a specific client id, use
-# mqttc = MyMQTTClass("client-id")
-# but note that the client id must be unique on the broker. Leaving the client
-# id parameter empty will generate a random id for you.
-
-
-
-# if rc return code not 0, try to reconnect 3 times and then exit if rc is still not 0
 
 def do_mqtt_stuff(hostname):
     mqttc = MyMQTTClass(client_id="python-test")
-    mqttc.connect("192.168.0.30", 1883, 60)    
-    mqttc.publish(topic = "pi/{}/coil_info".format(hostname), payload = "Hello World!", qos = 2, retain = False)
+    mqttc.connect("192.168.0.30", 1883, 60)
+    mqttc.publish(
+        topic="pi/{}/coil_info".format(hostname),
+        payload="Hello World!",
+        qos=2,
+        retain=False,
+    )
     mqttc.subscribe("pi/cw88/#", 2)
-    mqttc.loop_forever() 
-   
-    
-    
-    
+    mqttc.loop_forever()
+
+
 def do_other_stuff():
     while True:
         time.sleep(30)
         print("Hello World!")
-    
 
 
 def main():
@@ -111,8 +89,6 @@ def main():
     do_mqtt_stuff(hostname)
 
 
-    
-    
 if __name__ == "__main__":
     os.system("clear")
     main()
